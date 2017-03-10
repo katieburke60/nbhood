bd8ac25ebbb507b93d313bdf646832c962e5eb4c
johnshelb <jsnd11@gmail.com>
2017-03-10 15:53:47 -0500

    sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
 class SessionsController < ApplicationController
+
+  def create
+    account = Account.find_by(email: params[:email])
+    if account
+      member=account.member
+    else
+      redirect_to "/" alert: "Information invalid" and return
+    end
+    if member.authenticate(params[:password])
+      session[:account_id] = member.account_id
+      redirect_to member_path(member.id)
+    else
+      render "root/index", alert: "Information invalid" and return
+    end
+  end
 end
