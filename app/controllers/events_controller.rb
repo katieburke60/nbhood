0d8a82677bb3fd3acf2e4f0174a015ce6cb114a1
github username <github email address>
2017-03-10 16:05:32 -0500

    events nonsense

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,31 @@
 class EventsController < ApplicationController
+
+  def index
+    @events = []
+    if params[:name]
+      @events = Event.where("name like ?", "%#{params[:name]}%")
+    else
+      Event.all.each do |event|
+        @events << event if event.active
+      end
+    end
+  end
+
+  def show
+    @event = Event.find_by(id: params[:id])
+  end
+
+  def rsvp
+    @event = Event.find_by(id: params[:id])
+    @member = current_user
+    if @event.active
+      Rsvp.create(member_id: @member.id, event_id: @event.id)
+      flash[:message] = "RSVP successful. Event has been added to your calendar"
+      redirect_to @event
+    else
+      flash[:message] = "There was an error RSVPing - The Event is no longer active"
+      render '/events/show'
+    end
+
+  end
 end
