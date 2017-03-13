596306429e441744582d282a21749834078a381c
John <jsnd11@gmail.com>
2017-03-13 09:32:40 -0400

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,19 +3,22 @@ class ApplicationController < ActionController::Base
   helper_method :current_member, :is_logged_in?, :current_business, :business_logged_in?
 
     def current_member
-      current_member ||= Account.find_by_id(session[:account_id]).member
+      if is_logged_in?
+        current_member ||= Account.find_by_id(session[:account_id]).member
+      end
     end
 
      def is_logged_in?
-       current_member != nil
+       !!session[:account_id]
      end
 
      def current_business
-       #binding.pry
-       current_business ||= BusinessAccount.find_by_id(session[:business_account_id]).business
+       if business_logged_in?
+          current_business ||= BusinessAccount.find_by_id(session[:business_account_id]).business
+       end
      end
 
      def business_logged_in?
-       current_business != nil
+       !!session[:business_account_id]
      end
   end
