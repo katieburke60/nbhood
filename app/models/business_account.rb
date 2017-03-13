d1e94f9f37f75726b07dfc4ec6d01783a42aca30
John <jsnd11@gmail.com>
2017-03-13 09:36:46 -0400

    Update business_account.rb

--- app/models/business_account.rb
+++ app/models/business_account.rb
@@ -1,6 +1,6 @@
 class BusinessAccount < ApplicationRecord
   has_one :business
-  validates :email, presence: true
-  validates :password, presence: true
+  #validates :email, presence: true
+  #validates :password, presence: true
   has_secure_password
 end
