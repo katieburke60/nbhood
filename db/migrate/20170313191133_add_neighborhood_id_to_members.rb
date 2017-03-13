05aacbdb0b886a961e15e1c3a815d12fcd9ea3f1
johnshelb <jsnd11@gmail.com>
2017-03-13 16:05:46 -0400

    fixed migrations

--- db/migrate/20170313191133_add_neighborhood_id_to_members.rb
+++ db/migrate/20170313191133_add_neighborhood_id_to_members.rb
@@ -1,5 +1,5 @@
 class AddNeighborhoodIdToMembers < ActiveRecord::Migration[5.0]
   def change
-    add_reference :neighborhoods, :members, foreign_key: true
+    add_reference :members, :neighborhood, foreign_key: true
   end
 end
